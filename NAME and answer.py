58adba4e65065d446a01af32ae4b139bb62e1424
Tsielo Mudau <tsielo.mudau@team.itvarsity.org>
2021-11-09 02:14:37 +0200

    answer

---
+++ NAME and answer.py
@@ -0,0 +1,8 @@
+name = input("Enter name:")
+print("name is: " + name)
+number = int(input("what is 5+10: "))
+
+if number == 15:
+    print("You are a correct!")
+else:
+    print("You are incorrect!")
